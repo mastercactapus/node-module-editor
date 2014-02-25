c23d3845d463b22cccc141578acc2abff2069fb9
Nathan Caza <mastercactapus@gmail.com>
2014-02-24 22:18:10 -0600

    keep browser in sync with fs

--- components/file-browser.js
+++ components/file-browser.js
@@ -1,5 +1,8 @@
 var Backbone = require("backbone");
 var $ = require("jquery");
+var fs = require("fs");
+var path = require("path");
+var _ = require("lodash");
 
 var core = require("../lib/core");
 var FileBrowserView = require("../lib/file-browser");
@@ -15,9 +18,46 @@ filesView.on("file_clicked", function(file, filepath){
 	core.events.emit("load-file", file, filepath);
 });
 
+var watchers = {};
+
 //core events we care about
 core.events.on("set-dir", function(dirname) {
 	var files = scanner.scan(dirname);
-
 	filesCollection.set(files);
+
+	removeWatchers();
+	addWatchers(dirname, function(filename) {
+		//this api seems inconsistant, and inaccurate most of the time
+		if (fs.existsSync(filename)) {
+			filesCollection.add(scanner.file(dirname, filename), {merge: true});
+		} else {
+			filesCollection.remove({id: filename});
+		}
+	});
 });
+
+function removeWatchers() {
+	_.each(watchers, function(watcher){
+		watcher.close();
+	});
+	watchers = {};
+}
+function addWatchers(dirname, cb) {
+	watchers[dirname] = fs.watch(dirname, function(event, filename){
+		if (!watchers[filename] && fs.existsSync(filename) && fs.statSync(filename).isDirectory()) {
+			addWatchers(path.resolve(dirname, filename), cb);
+		} else if (watchers[filename] && !fs.existsSync(filename)) {
+			watchers[filename].close();
+			delete watchers[filename];
+		}
+
+		cb(path.resolve(dirname, filename));
+	});
+
+	fs.readdirSync(dirname).forEach(function(file){
+		file = path.resolve(dirname, file);
+		if (fs.statSync(file).isDirectory()) {
+			addWatchers(file, cb);
+		}
+	});
+}
