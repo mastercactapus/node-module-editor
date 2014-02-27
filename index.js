773bbce5c2ef907ab0bb4cecbfbeb2a640b6e3b2
Nathan Caza <mastercactapus@gmail.com>
2014-02-26 23:46:40 -0600

    find root of module given arg or PWD

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require("./lib/main");
+var gui = require("nw.gui");
+var fs = require("fs");
+var path = require("path");
+var initDir = gui.App.argv[0] || process.env.PWD;
+initDir = path.resolve(initDir);
+
+function findPackage(dir) {
+	var newPath = path.dirname(dir);
+	if (fs.existsSync(path.join(dir, "package.json"))) {
+		return dir;
+	} else if (newPath !== dir) {
+		return findPackage(newPath);
+	} else {
+		throw new Error("Could not find package.json");
+	}
+}
+
+
+require("./lib/main")( findPackage(initDir) );
